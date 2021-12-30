bd5f8e452c4231d19c1e576d95ad1741fe2a5679
lillyleiran <lilly.leiran@gmail.com>
2021-12-29 21:49:37 -0700

    update game

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,3 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+// opens an input field and assigns entered data to the variable playerName
+var playerName = window.prompt("What's your robot's name?");
+console.log(playerName);
+// does math
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
 
-kjhzskfhasdlf
+// creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight ();
